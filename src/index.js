180832aa07882b57f63c539387e51cb168fee15f
nostrss <jintagi@gmail.com>
2022-09-06 00:03:43 +0900

    3-1

--- src/index.js
+++ src/index.js
@@ -1,109 +1,122 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
+// const add = document.getElementById('add');
+// const minus = document.getElementById('minus');
+// const number = document.querySelector('span');
 
-const ADD = 'ADD';
-const MINUS = 'MINUS';
+// const ADD = 'ADD';
+// const MINUS = 'MINUS';
 
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
-};
-const countStore = createStore(countModifier);
+// const countModifier = (count = 0, action) => {
+//   switch (action.type) {
+//     case ADD:
+//       return count + 1;
+//     case MINUS:
+//       return count - 1;
+//     default:
+//       return count;
+//   }
+// };
+// const countStore = createStore(countModifier);
 
-const onChange = () => {
-  number.innerText = countStore.getState();
-};
+// const onChange = () => {
+//   number.innerText = countStore.getState();
+// };
 
-countStore.subscribe(onChange);
+// countStore.subscribe(onChange);
 
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
-};
+// const handleAdd = () => {
+//   countStore.dispatch({ type: ADD });
+// };
 
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
+// const handleMinus = () => {
+//   countStore.dispatch({ type: MINUS });
+// };
 
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+// add.addEventListener('click', handleAdd);
+// minus.addEventListener('click', handleMinus);
 
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const ul = document.querySelector('ul');
+// const form = document.querySelector('form');
+// const input = document.querySelector('input');
+// const ul = document.querySelector('ul');
 
-const ADD_TODO = 'ADD_TODO';
-const DELETE_TODO = 'DELETE_TODO';
+// const ADD_TODO = 'ADD_TODO';
+// const DELETE_TODO = 'DELETE_TODO';
 
-const reducer = (state = [], action) => {
-  console.log(action);
-  switch (action.type) {
-    case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
-    case DELETE_TODO:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
+// const reducer = (state = [], action) => {
+//   console.log(action);
+//   switch (action.type) {
+//     case ADD_TODO:
+//       return [...state, { text: action.text, id: Date.now() }];
+//     case DELETE_TODO:
+//       return state.filter((toDo) => toDo.id !== action.id);
+//     default:
+//       return state;
+//   }
+// };
 
-const addToDo = (text) => {
-  return {
-    type: ADD_TODO,
-    text,
-  };
-};
+// const addToDo = (text) => {
+//   return {
+//     type: ADD_TODO,
+//     text,
+//   };
+// };
 
-const deleteToDo = (id) => {
-  return {
-    type: DELETE_TODO,
-    id,
-  };
-};
+// const deleteToDo = (id) => {
+//   return {
+//     type: DELETE_TODO,
+//     id,
+//   };
+// };
 
-const store = createStore(reducer);
+// const store = createStore(reducer);
 
-store.subscribe(() => console.log(store.getState()));
+// store.subscribe(() => console.log(store.getState()));
 
-const dispatchAddToDo = (text) => {
-  store.dispatch(addToDo(text));
-};
+// const dispatchAddToDo = (text) => {
+//   store.dispatch(addToDo(text));
+// };
 
-const dispatchDeleteToDo = (event) => {
-  const id = parseInt(event.target.parentNode.id);
-  store.dispatch(deleteToDo(id));
-};
+// const dispatchDeleteToDo = (event) => {
+//   const id = parseInt(event.target.parentNode.id);
+//   store.dispatch(deleteToDo(id));
+// };
 
-const paintToDos = () => {
-  const toDos = store.getState();
-  ul.innerHTML = '';
-  toDos.forEach((toDo) => {
-    const li = document.createElement('li');
-    const btn = document.createElement('button');
-    btn.innerText = 'DEL';
-    btn.addEventListener('click', dispatchDeleteToDo);
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-    li.appendChild(btn);
-    ul.appendChild(li);
-  });
-};
+// const paintToDos = () => {
+//   const toDos = store.getState();
+//   ul.innerHTML = '';
+//   toDos.forEach((toDo) => {
+//     const li = document.createElement('li');
+//     const btn = document.createElement('button');
+//     btn.innerText = 'DEL';
+//     btn.addEventListener('click', dispatchDeleteToDo);
+//     li.id = toDo.id;
+//     li.innerText = toDo.text;
+//     li.appendChild(btn);
+//     ul.appendChild(li);
+//   });
+// };
 
-store.subscribe(paintToDos);
+// store.subscribe(paintToDos);
 
-const onSubmit = (e) => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = '';
-  dispatchAddToDo(toDo);
-};
+// const onSubmit = (e) => {
+//   e.preventDefault();
+//   const toDo = input.value;
+//   input.value = '';
+//   dispatchAddToDo(toDo);
+// };
 
-form.addEventListener('submit', onSubmit);
+// form.addEventListener('submit', onSubmit);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import store from './store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/routes/Home.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+export default function Home() {
+  const [text, setText] = useState('');
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(text);
+  };
+
+  return (
+    <>
+      <h1>ToDo</h1>
+      <form onSubmit={onSubmit}>
+        <input type='text' value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul></ul>
+    </>
+  );
+}
